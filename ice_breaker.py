ffc79f48ae7422020b28eb28d433d8ffee56cd2b
Hyunjae Lee <linked0@gmail.com>
2025-07-04 08:37:48 +0900

    feat: add rag, react samples

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from dotenv import load_dotenv
 import os
 from langchain_core.prompts import PromptTemplate
@@ -7,12 +9,13 @@ from langchain_core.output_parsers import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from output_parsers import summary_parser
+from output_parsers import Summary
 
 # information = """
 #         Bill Clinton and his wife
 # """
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
@@ -31,10 +34,11 @@ def ice_break_with(name: str) -> str:
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = summary_prompt_template | llm | summary_parser
-    res = chain.invoke(input={"information": linkedin_data})
+    res:Summary = chain.invoke(input={"information": linkedin_data})
 
     print("#####################################################")
     print(res)
+    return res, linkedin_data.get("protoUrl")
 
 if __name__ == "__main__":
     load_dotenv()
